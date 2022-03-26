13d48d883ed3d2bc823c09516f3e89602de3d4d5
Diego Gean da Fé <diegogeandafe@gmail.com>
2022-03-26 18:32:58 -0300

    Atualizado o handler CryptoHandler

--- src/main/kotlin/com/open/crypto/api/handler/CryptoHandler.kt
+++ src/main/kotlin/com/open/crypto/api/handler/CryptoHandler.kt
@@ -8,7 +8,6 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @Handler
 class CryptoHandler {
